942d314b15658a3d54a25bdd0a362e06d3bd09d6
beatrizaparecida01 <aparecida.bealmeida@gmail.com>
2026-04-16 21:56:47 -0300

    parte 2 contador

--- lib/main.dart
+++ lib/main.dart
@@ -1,59 +1,126 @@
 import 'package:flutter/material.dart';
 
-void main()
-{
-  runApp(const MyApp());
+void main() {
+  runApp(MyApp());
 }
+
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return MaterialApp(
+  Widget build(BuildContext context) {
+    return const MaterialApp(
       home: HomePage(),
     );
   }
 }
+
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
+  void decrement() {
+    print('decrement');
+  }
 
-  void decrement(){ print('decrement');}
-  void increment(){ print('increment');}
+  void increment() {
+    print('increment');
+  }
 
   @override
-  Widget build(BuildContext context){
-    return const Scaffold(
-      body: Column(
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          image: DecorationImage(image: AssetImage('assets/images/cidade.jpg'),
+          fit: BoxFit.cover,
+          ),
+        ),
+        child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Text("Pode Entrar",
-          style: TextStyle(
-            fontSize: 26,
-            color: Color.fromARGB(255, 4, 87, 154),
-            fontWeight: FontWeight.w700
-          ),),
+          Text(
+            "Pode entrar",
+            style: TextStyle(
+                fontSize: 26,
+                color: Color.fromARGB(255, 4, 87, 154),
+                fontWeight: FontWeight.w700
+                ),
+          ),
+          Text(
+            "0",
+            style: TextStyle(
+                fontSize: 26,
+                color: Color.fromARGB(255, 4, 87, 154),
+                fontWeight: FontWeight.w700),
+          ),
+
+            const Padding(padding: EdgeInsets.all(40),
+            child: Text('0',
+              style: TextStyle(
+                fontSize: 100,
+                color: Color.fromARGB(255, 4, 87, 154),
+              ),
+            ),
+            ),   
+
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              TextButton(onPressed: decrement,
-              child: Text('Sair'),),
-              TextButton(onPressed: increment, child: Text('Entrar'),),
+              TextButton(
+                onPressed: decrement,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: const Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                child: Text(
+                  'Sair',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              SizedBox(
+                width: 32,
+              ),
+              TextButton(
+                onPressed: increment,
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  fixedSize: const Size(100, 100),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(24),
+                  ),
+                ),
+                child: Text(
+                  'Entrar',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
             ],
           )
         ],
       ),
+      )
     );
   }
 }
 
+class MainApp extends StatelessWidget {
+  const MainApp({super.key});
   @override
-  Widget build(BuildContext context)
-{
-  return const MaterialApp(
-    home: Scaffold(
-      backgroundColor: Colors.pink,
-      body: Center(
-        child: Text('Hello World!'),
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Text('Hello World!'),
+        ),
       ),
-    ),
-  );
+    );
+  }
 }
