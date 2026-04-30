922b70ecbc1f734c4058ce195e4f451d7631ff05
beatrizaparecida01 <aparecida.bealmeida@gmail.com>
2026-04-30 20:24:08 -0300

    parte4

--- lib/main.dart
+++ lib/main.dart
@@ -1,126 +1,112 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(MyApp());
+void main(){
+  runApp(
+    MyApp()
+  );
 }
-
-class MyApp extends StatelessWidget {
-  MyApp({super.key});
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: HomePage(),
-    );
+  Widget build(BuildContext content){
+    return MaterialApp( 
+      debugShowCheckedModeBanner: false,    
+      home: HomePage(), 
+  );
   }
 }
+class HomePage extends StatefulWidget {
+  HomePage({super.key});
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-  void decrement() {
-    print('decrement');
-  }
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
 
-  void increment() {
-    print('increment');
-  }
+class _HomePageState extends State<HomePage> {
+  int contador = 0;
+
+  void decrement(){
+    setState(() {
+      contador--;
+    });       
+      print(contador);
+     }
+
+   void increment(){
+    setState(() {
+      contador++;
+    });     
+      print(contador);
+     }
+
+    bool get isEmpty => contador == 0;
+    bool get isFull => contador == 10;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        decoration: BoxDecoration(
-          image: DecorationImage(image: AssetImage('assets/images/cidade.jpg'),
-          fit: BoxFit.cover,
-          ),
+        decoration: BoxDecoration(         
+          image: DecorationImage(
+            image: AssetImage('assets/images/image.jpg'),
+            fit: BoxFit.cover,
+            ),
         ),
         child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Text(
-            "Pode entrar",
-            style: TextStyle(
-                fontSize: 26,
-                color: Color.fromARGB(255, 4, 87, 154),
-                fontWeight: FontWeight.w700
-                ),
+            isFull ? 'Lotado' : 'Pode Entrar',
+          style: TextStyle(
+            fontSize: 26,
+            color: isFull ? Colors.red : Color.fromARGB(255, 4, 87, 154),
+            fontWeight: FontWeight.w700
           ),
-          Text(
-            "0",
-            style: TextStyle(
-                fontSize: 26,
-                color: Color.fromARGB(255, 4, 87, 154),
-                fontWeight: FontWeight.w700),
           ),
-
-            const Padding(padding: EdgeInsets.all(40),
-            child: Text('0',
-              style: TextStyle(
-                fontSize: 100,
-                color: Color.fromARGB(255, 4, 87, 154),
-              ),
-            ),
-            ),   
-
+        
+          Padding(padding: EdgeInsets.all(24),
+          child: Text(
+            contador.toString(),
+           style: TextStyle(
+            fontSize: 26,
+            color: isFull ? Colors.red : Color.fromARGB(255, 4, 87, 154),
+            fontWeight: FontWeight.w700
+          ),
+          ),
+          ),
+           
           Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               TextButton(
-                onPressed: decrement,
-                style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
+                onPressed: isEmpty ? null : decrement,
+              style: TextButton.styleFrom(
+                  backgroundColor:isEmpty ? Colors.blue.withOpacity(0.2) : Colors.blue,
                   fixedSize: const Size(100, 100),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-                child: Text(
-                  'Sair',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
+                  ),),
+               child: Text('Saiu'),
               ),
-              SizedBox(
-                width: 32,
-              ),
+
+              SizedBox(width: 32,),
+
               TextButton(
-                onPressed: increment,
+                onPressed: isFull ? null : increment,                 
                 style: TextButton.styleFrom(
-                  backgroundColor: Colors.blue,
+                  backgroundColor: isFull ? Colors.blue.withOpacity(0.2) : Colors.blue,
                   fixedSize: const Size(100, 100),
                   shape: RoundedRectangleBorder(
                     borderRadius: BorderRadius.circular(24),
-                  ),
-                ),
-                child: Text(
-                  'Entrar',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 16,
-                  ),
-                ),
+                  ),),
+               child: Text('Entrar'),
               ),
             ],
-          )
+          ),
         ],
       ),
       )
-    );
-  }
-}
-
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
     );
   }
 }
